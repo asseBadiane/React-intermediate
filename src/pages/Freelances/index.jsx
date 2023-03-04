6e4f27af22a322c4e90d9f9193941b689ce371b9
Malick <malick@hp-probook-450-g2.home>
2023-03-04 22:38:06 +0000

    I used api calls with useEffect ...

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import DefaultPicture from '../../assets/profile.png'
+// import DefaultPicture from '../../assets/profile.png'
 import Card from '../../components/Card'
 import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atoms'
 
 const CardsContainer = styled.div`
   display: grid;
@@ -24,46 +26,84 @@ const PageSubtitle = styled.h2`
   text-align: center;
   padding-bottom: 30px;
 `
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
 
-const freelanceProfiles = [
-    {
-        name: 'Jane Doe',
-        jobTitle: 'Devops',
-        picture: DefaultPicture,
-    },
-    {
-        name: 'John Doe',
-        jobTitle: 'Developpeur frontend',
-        picture: DefaultPicture,
-    },
-    {
-        name: 'Jeanne Biche',
-        jobTitle: 'Développeuse Fullstack',
-        picture: DefaultPicture,
-    },
-]
+// const freelanceProfiles = [
+//     {
+//         name: 'Jane Doe',
+//         jobTitle: 'Devops',
+//         picture: DefaultPicture,
+//     },
+//     {
+//         name: 'John Doe',
+//         jobTitle: 'Developpeur frontend',
+//         picture: DefaultPicture,
+//     },
+//     {
+//         name: 'Jeanne Biche',
+//         jobTitle: 'Développeuse Fullstack',
+//         picture: DefaultPicture,
+//     },
+// ]
 
 function Freelance() {
-    return (
-        <div>
-            <PageTitle>Trouvez votre prestataire</PageTitle>
-            <PageSubtitle>
-                Chez Shiny nous rèunissons les meilleurs profils pour vous
-            </PageSubtitle>
-            <CardsContainer>
-              <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
-              {freelanceProfiles.map((profile, index) => (
-                <Card 
-                    key={`${profile.name}-${index}`}
-                    label={profile.jobTitle}
-                    picture={profile.picture}
-                    title={profile.title}
-                    />
-              ))}
-            </CardsContainer>
-        </div>
+
+    const [ isDataLoading, setDataLoading ] = useState(false)
+    const [error, setError] = useState(false)
+    const [freelancersList, setFreelancesList] = useState([])
+
     
-    )
+  useEffect(() => {
+    async function fetchFreelances() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/freelances`)
+        const { freelancersList } = await response.json()
+        setFreelancesList(freelancersList)
+      } catch (err) {
+        console.log('===== error =====', err)
+        setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+      fetchFreelances()
+        }, [])
+
+        if (error) {
+          return <span>Oups il y a eu un problème</span>
+        }
+
+    return (
+
+      <div>
+        <PageTitle>Trouvez votre prestataire</PageTitle>
+        <PageSubtitle>
+          Chez Shiny nous réunissons les meilleurs profils pour vous.
+        </PageSubtitle>
+        {isDataLoading ? (
+          <LoaderWrapper>
+            <Loader />
+          </LoaderWrapper>
+        ) : (
+          <CardsContainer>
+            {freelancersList.map((profile, index) => (
+              <Card
+                key={`${profile.name}-${index}`}
+                label={profile.job}
+                title={profile.name}
+                picture={profile.picture}
+              />
+            ))}
+          </CardsContainer>
+        )}
+    </div>
+  )
+    
+    
 }
 
 export default Freelance
