d3a5f7c421200b6de0c13ce9c4cc089f01a5cbc2
ReddingCode <badianeasse99@gmail.com>
2023-03-06 12:26:45 +0000

    I used the context with 'useContext'. Le Contexte est une fonctionnalité de React permettant de partager le state entre plusieurs composants parents et enfants, à l'aide d'un Provider.useContext  est un hook permettant de se brancher très simplement au Contexte, et donc d'accéder au State partagé.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
+// import { createGlobalStyle } from 'styled-components';
 import Error from './components/Error';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Freelance from './pages/Freelances';
 import Home from './pages/Home';
 import Result from './pages/Result';
 import Survey from './pages/Survey'
+import { SurveyProvider, ThemeProvider } from './utils/style/context';
+import GlobalStyle from './utils/style/GlobalStyle';
 
 
-const GlobalStyle = createGlobalStyle`
-    div {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-`
+// const GlobalStyle = createGlobalStyle`
+//     div {
+//         font-family: 'Trebuchet MS', Helvetica, sans-serif;
+//     }
+// `
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/survey/:questionNumber' element={<Survey />} />
-        <Route path='/freelance' element={<Freelance />} />
-        <Route path='/result' element={<Result />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/survey/:questionNumber' element={<Survey />} />
+            <Route path='/freelance' element={<Freelance />} />
+            <Route path='/result' element={<Result />} />
+            <Route path='*' element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react"
+import { SurveyContext } from "../../utils/style/context"
 
 function Result() {
+    const { answers } = useContext(SurveyContext)
+    console.log(answers)
     return (
         <div>
             <h1>Les Résultats</h1>
