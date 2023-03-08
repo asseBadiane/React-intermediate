18df802ce2d792bc519c0714838a907fe79d77e6
ReddingCode <badianeasse99@gmail.com>
2023-03-08 16:45:14 +0000

    Les anciens syntaxe de React avec les class. Les composants classe sont apparus en même temps que les class  en JavaScript.
    Un composant classe est déclaré avec  class NomDuComposant extends Component   .
    
    render  est appelé à chaque fois qu’une mise à jour a lieu.
    
    Le state est mis à jour avec setState  , auquel on passe un objet en paramètre.
    
    On accède aux props et au state avec this.props  et this.state

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,6 @@
 // La biblothèque "prop-types" nous permet de sécuriser nos props 
 import PropTypes from 'prop-types'
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import styled from 'styled-components'
 import DefaultPicture from '../../assets/profile.png'
 import { useTheme } from '../../utils/hooks'
@@ -38,33 +38,67 @@ const CardWrapper = styled.div`
         box-shadow: 2px 2px 10px #e2e3e9;
     }
 `
-function Card({label, title, picture}) {
-    const { theme } = useTheme()
-    const [isFavorite, setIsFavorite] = useState(false)
-    const star = isFavorite ? '⭐️' : '' 
+// function Card({label, title, picture}) {
+//     const { theme } = useTheme()
+//     const [isFavorite, setIsFavorite] = useState(false)
+//     const star = isFavorite ? '⭐️' : '' 
     
-    return (
-        <CardWrapper theme={theme} onClick={() => setIsFavorite(!isFavorite)}>
-            <CardLabel theme={theme}>{label}</CardLabel>
-            <CardImage src={picture} alt="freelance" />
-            <CardTitle theme={theme}>
-                {star} {title} {star}
-            </CardTitle>
-        </CardWrapper>
-    )
+//     return (
+//         <CardWrapper theme={theme} onClick={() => setIsFavorite(!isFavorite)}>
+//             <CardLabel theme={theme}>{label}</CardLabel>
+//             <CardImage src={picture} alt="freelance" />
+//             <CardTitle theme={theme}>
+//                 {star} {title} {star}
+//             </CardTitle>
+//         </CardWrapper>
+//     )
     
+// }
+
+// Fonction Card en mode class
+class Card extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isFavorite: false
+        }
+    }
+
+    setFavorite = () => {
+        this.setState({ isFavorite: !this.state.isFavorite })
+    } 
+
+    render() {
+        const { theme, label, picture, title } = this.props
+        const { isFavorite } = this.state
+        const star = isFavorite ? '⭐️' : '' 
+
+        return (
+            <CardWrapper theme={theme} onClick={this.isFavorite}>
+                <CardLabel theme={theme}>{label}</CardLabel>
+                <CardImage src={picture} alt="freelance" />
+                <CardTitle theme={theme}>
+                  {star} {title} {star}
+                </CardTitle>
+            </CardWrapper>
+        )
+    }
 }
+
+
 // Exiger un prop avec isRequired
 Card.propTypes = {
     label: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     picture: PropTypes.string.isRequired,
+    theme: PropTypes.string.isRequired,
   }
 // Utiliser defaulProps pour Eviter les erreurs 
 Card.defaultProps = {
     label: '',
     title: '',
     picture: DefaultPicture,
+    theme: 'light',
   }
 
 export default Card
